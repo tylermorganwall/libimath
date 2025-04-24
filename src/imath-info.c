7aa829bf8d72bbbd5cd8389525d1efef01948f0d
tylermorganwall <tylermorganwall@users.noreply.github.com>
2025-04-23 23:14:38 -0400

    -Update COPYRIGHTS

--- src/imath-info.c
+++ src/imath-info.c
@@ -4,7 +4,6 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 #include <R_ext/Print.h> 
-#include <R_ext/Memory.h> 
 
 #include <Imath/ImathConfig.h>
 
@@ -21,7 +20,7 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
-void R_init_libdeflatewrapper(DllInfo *dll) {
+void R_init_libimathwrapper(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
